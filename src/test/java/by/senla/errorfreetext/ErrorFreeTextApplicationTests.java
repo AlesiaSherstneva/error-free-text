9b746ada376095572ffdd0b79a6004d437d03b35
Alesia Sherstneva <alesiasherstneva@gmail.com>
2026-02-10 19:12:52 +0300

    Create basic project

---
+++ src/test/java/by/senla/errorfreetext/ErrorFreeTextApplicationTests.java
@@ -0,0 +1,11 @@
+package by.senla.errorfreetext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorFreeTextApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
